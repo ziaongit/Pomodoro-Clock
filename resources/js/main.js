77bb167d68e7793b65f1640530346e231883c62d
Zia <zia_gt@yahoo.com>
2017-08-04 02:27:06 +0200

    Timer change

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,4 @@
 $(function(){
-    var counter = 0;
-    var timeLeft = 60;
-    var timer = $('#timer');
-    timer.html(counter);
-    function timeIt() {
-        counter++;
-        timer.html(timeLeft-counter);
-    }
-
-    setInterval(timeIt, 1000);
+    
     
 });
